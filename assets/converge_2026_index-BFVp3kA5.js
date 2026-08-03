f75c0cff8d53ce9f97a940ef3621e2a128bddd62
funmu <muralirk@gmail.com>
2026-08-03 12:53:31 -0700

    pulbish updated catalog with images

--- assets/converge_2026_index-BFVp3kA5.js
+++
@@ -1,1 +0,0 @@
-/* empty css               */import{t as e}from"./event-helpers-BPxgm6dD.js";async function t(){try{let t=await fetch(`./catalog.json`);t.ok?e(`catalogGrid`,await t.json()):console.error(`Failed to load product catalog JSON:`,t.statusText)}catch(e){console.error(`Error fetching product catalog JSON:`,e)}}t();
